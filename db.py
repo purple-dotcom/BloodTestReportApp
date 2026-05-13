8226894d8d1c4d0914a937ec5eb086124bba3f11
Eli Shane <ayaaan172@gmail.com>
2026-05-13 23:28:26 +0530

    added gunicorn

--- db.py
+++ db.py
@@ -7,6 +7,7 @@ load_dotenv()
 def get_connection():
     return psy.connect(
         host = os.getenv("DB_HOST"),
+        port = os.getenv("DB_PORT", 5432),
         database = os.getenv("DB_NAME"),
         user = os.getenv("DB_USER"), 
         password = os.getenv("DB_PASSWORD")
